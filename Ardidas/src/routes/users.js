66ef46695bc4fde9b0d142cc62760bb1b869d265
LeonelAnchaba <leonelanchaba@gmail.com>
2024-01-16 23:27:09 -0300

    Modularización de multer para dejar mas prolijo el archivo de rutas de usuarios

--- Ardidas/src/routes/users.js
+++ Ardidas/src/routes/users.js
@@ -1,25 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {formRegister, register, formLogin, login} = require('../controllers/usersControllers');
-const multer = require("multer");
-const path = require("path");
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "../../public/images/users"))
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`)
-    }
-})
-
-const uploadFile = multer({ 
-    storage,
-     fileFilter: function(_req, file, cb) {
-         let type = file.mimetype.startsWith("image");
-         type ? cb(null, true) : cb(null, false)
-     }
-})
+const uploadFile = require("../validations/imageUser")
 
 
 router
