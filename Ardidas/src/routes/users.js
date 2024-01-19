7d1efb9c633c00c2d2753ec5dc3096a0384f6096
LeonelAnchaba <leonelanchaba@gmail.com>
2024-01-19 02:11:35 -0300

    middlewares para chequear si está activa una sesión y para conocer si el que navega es administrador o usuario creados en una carpeta propia y aplicados en las rutas con las que contamos de momento. En cuanto tengamos la sección de actualizar perfil, será agregado

--- Ardidas/src/routes/users.js
+++ Ardidas/src/routes/users.js
@@ -4,10 +4,12 @@ const {formRegister, register, formLogin, login} = require('../controllers/users
 const uploadFile = require("../validations/imageUser")
 const registerValidation = require('../validation/validationRegister');
 const loginValidation = require('../validation/validationLogin')
+// const sessionValidate = require("../middleware/sessionValidate");
+// const isAdmin = require("../middleware/isAdminValidate");
 
 
 router
-
+//al validar la session en el controlador no estamos haciendo que funcione como middleware
 .get('/register', formRegister)
 .post("/register", uploadFile.single("image"),registerValidation, register)
 
