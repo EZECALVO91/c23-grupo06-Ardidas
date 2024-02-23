58efac5e3cb815929e2755f501bd29d3966964bd
LeonelAnchaba <leonelanchaba@gmail.com>
2024-02-23 13:34:08 -0300

    Listo el dashboard, ya cargan las imagenes

--- Ardidas/src/controllers/productsControllers.js
+++ Ardidas/src/controllers/productsControllers.js
@@ -26,29 +26,7 @@ const productsController = {
       })
       .catch((error) => console.log(error));
   },
-  productCart: (req, res) => {
-    const { id } = req.params;
-    const products = getJson("product");
-    const product = products.find((producto) => producto.id == id);
-    res.render("products/productCart", {
-      title: "Carrito",
-      product,
-      usuarioLogeado: req.session.usuarioLogin,
-    });
-  },
   productDetail: (req, res) => {
-    // const products = getJson("product");
-    // const { id } = req.params;
-    // const product = products.find((producto) => producto.id == id);
-    // const productOff = products.filter(
-    //   (product) => product.category === "oferta"
-    // );
-    // res.render("products/productDetail", {
-    //   title: "Detalle",
-    //   product,
-    //   productOff,
-    //   usuarioLogeado: req.session.usuarioLogin,
-    // });
     let product = db.Product.findByPk(req.params.id,{
         attributes: ["id","name","price","description","id_category_product","id_size","id_color",
         ],
@@ -56,6 +34,10 @@ const productsController = {
           {
             association: "Image_products",
             attributes: ["id", "name", "path", "id_product"],
+          },
+          {
+            association: "Category_products",
+            attributes: ["id", "category"],
           },
         ],
       });
@@ -74,24 +56,14 @@ const productsController = {
         const product = products.find(producto => producto.id == id)
         res.render('products/productCart', { title: "Carrito", product,usuarioLogeado: req.session.usuarioLogin })
     },
-    productDetail: (req, res) => {
-        const products = getJson("product");
-        const { id } = req.params
-        const product = products.find(producto => producto.id == id)
-        const productOff = products.filter(product => product.category === "oferta")
-        res.render('products/productDetail', { title: "Detalle", product, productOff,usuarioLogeado: req.session.usuarioLogin })
-    },
     dashboard: (req, res) => {
-        // const products = getJson("product");
-        // res.render('products/dashboard', { title: "Dashboard", products,usuarioLogeado: req.session.usuarioLogin });
         let products = db.Product.findAll({
             attributes:["id","name","price","description","id_category_product", "id_size", "id_color"],
             include: [
                 {association:"Image_products",
                 attributes:["id","name","path","id_product"]
             }
-            ]
-            
+            ]     
         })
         
         Promise.all([products])
@@ -165,22 +137,6 @@ const productsController = {
         res.redirect(`/products/detalle/${id}`);
     },
     destroy: (req, res) => {
-        //  const { id } = req.params;
-        //  const products = getJson("product");
-
-        //  let product = products.find(product => product.id == id);
-        //  let productClear = products.filter(product => product.id !== +req.params.id);
-        //  if (product.imagen == "default-image.png") {
-        //      setJson(productClear, "product");
-        //      res.redirect('/products/dashboard')
-        //  } else {
-        //  fs.unlink(`./public/images/products/${product.imagen}`, (err) => {
-        //      if (err) throw err
-        //      console.log(`borre el archivo ${product.image}`)
-        //    })
-        //    setJson(productClear, "product");
-        //    res.redirect('/products/dashboard')
-        //  }
          db.Product.destroy({
              where : {
                  id : req.params.id
@@ -188,8 +144,6 @@ const productsController = {
          })
          .then(()=>res.redirect("/products/dashboard"))
          .catch(error => console.log(error))
-          
-    
      }
 }
 
