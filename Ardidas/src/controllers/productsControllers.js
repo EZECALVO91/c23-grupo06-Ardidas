d55f81e158cd3536618ccec4311b1fc4e959f74f
LeonelAnchaba <leonelanchaba@gmail.com>
2024-02-23 13:00:13 -0300

    Sessión productos y detalle de productos completa, ya cargan con su imagen. Falta resolver lo del stock en cuanto tengamos la base con su lógica terminada

--- Ardidas/src/controllers/productsControllers.js
+++ Ardidas/src/controllers/productsControllers.js
@@ -1,126 +1,168 @@
-const fs = require('fs')
+const fs = require("fs");
 const { setJson, getJson } = require("../utility/jsonMethod");
 const db = require("../database/models");
 const { Sequelize } = require("../database/models");
 
-
 const productsController = {
-    index: (req, res) => {
+  index: (req, res) => {
+    let products = db.Product.findAll({
+      attributes: ["id","name","price","description","id_category_product","id_size","id_color",
+      ],
+      include: [
+        {
+          association: "Image_products",
+          attributes: ["id", "name", "path", "id_product"],
+        },
+      ],
+    });
 
-        let products = db.Product.findAll({
-            attributes:["id","name","price","description","id_category_product", "id_size", "id_color"],
-            include: [
-                {association:"Image_products",
-                attributes:["id","name","path","id_product"]
-            }
-            ]
-            
-        })
-        
-        Promise.all([products])
-        .then(([products]) => {
-            // return res.send(products)
-            res.render("products/products",{
-            title: "Ardidas",
+    Promise.all([products])
+      .then(([products]) => {
+        res.render("products/products", {
+          title: "Ardidas",
+          usuarioLogeado: req.session.usuarioLogin,
+          products,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
+  productCart: (req, res) => {
+    const { id } = req.params;
+    const products = getJson("product");
+    const product = products.find((producto) => producto.id == id);
+    res.render("products/productCart", {
+      title: "Carrito",
+      product,
+      usuarioLogeado: req.session.usuarioLogin,
+    });
+  },
+  productDetail: (req, res) => {
+    // const products = getJson("product");
+    // const { id } = req.params;
+    // const product = products.find((producto) => producto.id == id);
+    // const productOff = products.filter(
+    //   (product) => product.category === "oferta"
+    // );
+    // res.render("products/productDetail", {
+    //   title: "Detalle",
+    //   product,
+    //   productOff,
+    //   usuarioLogeado: req.session.usuarioLogin,
+    // });
+    let product = db.Product.findByPk(req.params.id,{
+        attributes: ["id","name","price","description","id_category_product","id_size","id_color",
+        ],
+        include: [
+          {
+            association: "Image_products",
+            attributes: ["id", "name", "path", "id_product"],
+          },
+        ],
+      });
+      Promise.all([product])
+        .then(([product]) => {
+            return res.render("products/productDetail",{
+            product,
             usuarioLogeado: req.session.usuarioLogin,
-            products,
-        
+            title: "Detalle"
         })})
         .catch(error=> console.log(error));
-    },
-    productCart: (req, res) => {
-        const { id } = req.params
-        const products = getJson("product");
-        const product = products.find(producto => producto.id == id)
-        res.render('products/productCart', { title: "Carrito", product,usuarioLogeado: req.session.usuarioLogin })
-    },
-    productDetail: (req, res) => {
-        const products = getJson("product");
-        const { id } = req.params
-        const product = products.find(producto => producto.id == id)
-        const productOff = products.filter(product => product.category === "oferta")
-        res.render('products/productDetail', { title: "Detalle", product, productOff,usuarioLogeado: req.session.usuarioLogin })
-    },
-    dashboard: (req, res) => {
-        const products = getJson("product");
-        res.render('products/dashboard', { title: "Dashboard", products,usuarioLogeado: req.session.usuarioLogin });
-    },
-    productLoad: (req, res) => {
-        res.render("products/productLoad", { title: "Crear",usuarioLogeado: req.session.usuarioLogin });
-    },
-    create: (req, res) => {
-        const file = req.file
-        const products = getJson("product");
-        const Nuevaid = Date.now();
-        const { nombre, talles, color, precio, descripcion, category  } = req.body;
-        console.log("Que llega?: " , req.body.color)
-        let nuevoJson = {
-            id: +Nuevaid,
-            nombre: nombre.trim(),
-            talles: talles >= 1 ? [talles] : talles,
-            color: Array.isArray(color) ? color : [color] ,
-            precio: +precio,
-            descripcion: descripcion.trim(),
-            imagen: file ? file.filename : "default-image.png",
-            category,   
-        }
-        let productoNuevo = [...products, nuevoJson]
-        setJson(productoNuevo, "product");
-        res.redirect("/products/dashboard");
-    },
-    //  loadStock: (req, res) => {
-    //     res.send("Holi")
-    //     // const { id } = req.params;
-    //     // const products = getJson("product");
-    //     // const product = products.find(elemento => elemento.id == id);
-    //     // res.render("products/loadStock", { title: "Stock", product, usuarioLogeado: req.session.usuarioLogin });
-    //  },
-    productEdit: (req, res) => {
-        const { id } = req.params;
-        const products = getJson("product");
-        const product = products.find(elemento => elemento.id == id);
-        res.render("products/productEdit", { title: "Editar producto", product,usuarioLogeado: req.session.usuarioLogin })
-    },
-    update: (req, res) => {
-        
-        const { id } = req.params;
-        const products = getJson("product");
-        const { nombre, precio, descripcion, imagen, category, talles, color } = req.body;
-        const nuevoArray = products.map(product => {
-            if (product.id == id)
-                return {
-                    id: +id,
-                    nombre: nombre.trim(),
-                    talles: talles >= 1 ? [talles] : talles,
-                    color :Array.isArray(color) ? color : [color],
-                    precio: +precio,
-                    descripcion: descripcion.trim(),
-                    imagen: imagen ? imagen : product.imagen,
-                    category,
-                }
 
-            return product
-        })
-        setJson(nuevoArray, "product")
-        res.redirect(`/products/detalle/${id}`);
-    },
-    destroy: (req, res) => {
-         const { id } = req.params;
-         const products = getJson("product");
 
-         let product = products.find(product => product.id == id);
-         let productClear = products.filter(product => product.id !== +req.params.id);
-         if (product.imagen == "default-image.png") {
-             setJson(productClear, "product");
-             res.redirect('/products/dashboard')
-         } else {
-         fs.unlink(`./public/images/products/${product.imagen}`, (err) => {
-             if (err) throw err
-             console.log(`borre el archivo ${product.image}`)
-           })
-           setJson(productClear, "product");
-           res.redirect('/products/dashboard')
-         }
+  },
+  dashboard: (req, res) => {
+    const products = getJson("product");
+    res.render("products/dashboard", {
+      title: "Dashboard",
+      products,
+      usuarioLogeado: req.session.usuarioLogin,
+    });
+  },
+  productLoad: (req, res) => {
+    res.render("products/productLoad", {
+      title: "Crear",
+      usuarioLogeado: req.session.usuarioLogin,
+    });
+  },
+  create: (req, res) => {
+    const file = req.file;
+    const products = getJson("product");
+    const Nuevaid = Date.now();
+    const { nombre, talles, color, precio, descripcion, category } = req.body;
+    console.log("Que llega?: ", req.body.color);
+    let nuevoJson = {
+      id: +Nuevaid,
+      nombre: nombre.trim(),
+      talles: talles >= 1 ? [talles] : talles,
+      color: Array.isArray(color) ? color : [color],
+      precio: +precio,
+      descripcion: descripcion.trim(),
+      imagen: file ? file.filename : "default-image.png",
+      category,
+    };
+    let productoNuevo = [...products, nuevoJson];
+    setJson(productoNuevo, "product");
+    res.redirect("/products/dashboard");
+  },
+  //  loadStock: (req, res) => {
+  //     res.send("Holi")
+  //     // const { id } = req.params;
+  //     // const products = getJson("product");
+  //     // const product = products.find(elemento => elemento.id == id);
+  //     // res.render("products/loadStock", { title: "Stock", product, usuarioLogeado: req.session.usuarioLogin });
+  //  },
+  productEdit: (req, res) => {
+    const { id } = req.params;
+    const products = getJson("product");
+    const product = products.find((elemento) => elemento.id == id);
+    res.render("products/productEdit", {
+      title: "Editar producto",
+      product,
+      usuarioLogeado: req.session.usuarioLogin,
+    });
+  },
+  update: (req, res) => {
+    const { id } = req.params;
+    const products = getJson("product");
+    const { nombre, precio, descripcion, imagen, category, talles, color } =
+      req.body;
+    const nuevoArray = products.map((product) => {
+      if (product.id == id)
+        return {
+          id: +id,
+          nombre: nombre.trim(),
+          talles: talles >= 1 ? [talles] : talles,
+          color: Array.isArray(color) ? color : [color],
+          precio: +precio,
+          descripcion: descripcion.trim(),
+          imagen: imagen ? imagen : product.imagen,
+          category,
+        };
+
+      return product;
+    });
+    setJson(nuevoArray, "product");
+    res.redirect(`/products/detalle/${id}`);
+  },
+  destroy: (req, res) => {
+    const { id } = req.params;
+    const products = getJson("product");
+
+    let product = products.find((product) => product.id == id);
+    let productClear = products.filter(
+      (product) => product.id !== +req.params.id
+    );
+    if (product.imagen == "default-image.png") {
+      setJson(productClear, "product");
+      res.redirect("/products/dashboard");
+    } else {
+      fs.unlink(`./public/images/products/${product.imagen}`, (err) => {
+        if (err) throw err;
+        console.log(`borre el archivo ${product.image}`);
+      });
+      setJson(productClear, "product");
+      res.redirect("/products/dashboard");
+    }
     //     db.Product.destroy({
     //         where : {
     //             id : req.params.id
@@ -128,9 +170,7 @@ const productsController = {
     //     })
     //     .then(()=>res.redirect("/products/dashboard"))
     //     .catch(error => console.log(error))
-          
-    
-     }
-}
+  },
+};
 
 module.exports = productsController;
