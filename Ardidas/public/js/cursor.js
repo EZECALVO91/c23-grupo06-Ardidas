8b8f89bab929147336e9d211b8c1a28dae69e3dd
EZECALVO91 <ezequielcalvo@live.com>
2024-03-21 21:42:37 -0300

    se agregaron algunos efectos y quedan algunas modificaciones.

---
+++ Ardidas/public/js/cursor.js
@@ -0,0 +1,6 @@
+document.addEventListener("mousemove", function(event){
+    let cursor = document.querySelector(".custom-cursor");
+            cursor.style.top = (event.clientY - cursor.offsetHeight / 2)+ "px";
+            cursor.style.left = (event.clientX - cursor.offsetWidth / 2)+ "px";
+
+})
