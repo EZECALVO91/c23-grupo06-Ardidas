5256eb8b608dfee2c9b1e9102e39896067c4f10d
EZECALVO91 <ezequielcalvo@live.com>
2024-04-06 22:04:15 -0300

    se creo la vista nueva para despues hacer los componentes con react, tambien se agregaron transiciones

--- Ardidas/src/controllers/dashboardReactControllers.js
+++ Ardidas/src/controllers/dashboardReactControllers.js
@@ -8,11 +8,91 @@ const { Op } = require("sequelize");
 const usersController = {
 // ---------------ACA EMPIEZA EL DASHBOARD DE REACT--------------------------------------------------------------------------------------------------------------
 
-    DashboardReact:(req, res) => {
-      res.render("./dashboardReact/dashboardReact", {title: "Dashboard", usuarioLogeado: req.session.usuarioLogin,}); // vista de formulario de login.
-    },
+// DashboardReact:  async (req, res) => {
+//     try {
+//         const ultimoProducto = await db.Product.findOne({
+//             include: [{
+//                 association: "Image_products"
+//             }],
+//             order: [['createdAt', 'DESC']],
+//         });
+
+//         if (!ultimoProducto) {
+//             return res.status(404).send("No se encontraron productos.");
+//         }
+
+//         res.render("./dashboardReact/dashboardReact", {title: "Último Producto",usuarioLogeado: req.session.usuarioLogin,product: ultimoProducto,});
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send("Ocurrió un error al cargar el último producto.");
+//     }
+// },
+
+// DashboardReact : async (req, res) => {
+//     try {
+//         const ultimoProducto = await db.Product.findOne({
+//             include: [{
+//                 association: "Image_products"
+//             }],
+//             order: [['createdAt', 'DESC']],
+//         });
+
+//         const categories = await db.Category_product.findAll({
+//             include: [{
+//                 model: db.Product,
+//                 as: 'Products', // Utilizamos el alias definido en la asociación
+//                 attributes: ['id', 'name']
+//             }]
+//         });
+
+//         res.render("./dashboardReact/dashboardReact", {
+//             title: "Dashboard",usuarioLogeado: req.session.usuarioLogin,product: ultimoProducto,categories: categories
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send("Ocurrió un error al cargar el panel de control.");
+//     }
+// },
+
+
+DashboardReact : async (req, res) => {
+    try {
+        const ultimoProducto = await db.Product.findOne({
+            include: [{
+                association: "Image_products"
+            }],
+            order: [['createdAt', 'DESC']],
+        });
+        const categories = await db.Category_product.findAll({
+                        include: [{
+                            model: db.Product,
+                            as: 'Products',
+                            attributes: ['id', 'name']
+                    }]
+            });
+
+            const products = await db.Product.findAll();
+
+        let categoriaConMasProductos;
+        let maximoProductos = 0;
+
+        categories.forEach(category => {
+            if (category.Products.length > maximoProductos) {
+                maximoProductos = category.Products.length;
+                categoriaConMasProductos = category;
+            }
+        });
+        res.render("./dashboardReact/dashboardReact", {title: "Dashboard",usuarioLogeado: req.session.usuarioLogin,product: ultimoProducto, categories: categories, maxCategories: categoriaConMasProductos, products:products
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Hubo un error en el panel de control.");
+    }
+
+},
 
 
 }
+
 
 module.exports = usersController;
