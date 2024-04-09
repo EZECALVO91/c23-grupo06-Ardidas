1ee93ae3843aaf0694e934d72124bd8f7e113188
EZECALVO91 <ezequielcalvo@live.com>
2024-04-08 23:37:17 -0300

    se creo la vista completa de dashboard para utilizarlo para hacer componentes de modulo, quedo funcional con info que se pidio, se creo css,ejs,rutas,controladores y se le sumaron las apis

--- Ardidas/src/controllers/dashboardReactControllers.js
+++ Ardidas/src/controllers/dashboardReactControllers.js
@@ -1,62 +1,65 @@
-const fs = require("fs");
-const path = require("path");
-const bcrypt = require("bcryptjs");
-const { validationResult } = require("express-validator");
 const db = require("../database/models");
-const { Op } = require("sequelize");
 
-const usersController = {
-// ---------------ACA EMPIEZA EL DASHBOARD DE REACT--------------------------------------------------------------------------------------------------------------
-DashboardReact : async (req, res) => {
-    try {
-        const ultimoProducto = await db.Product.findOne({
-            include: [{
-                association: "Image_products"
-            }],
-            order: [['createdAt', 'DESC']],
-        });
-        const categories = await db.Category_product.findAll({
-                        include: [{
-                            model: db.Product,
-                            as: 'Products',
-                            attributes: ['id', 'name']
-                    }]
+const dashboardController = {
+    DashboardReact: async (req, res) => {
+        try {
+            const ultimoProducto = await db.Product.findOne({
+                include: [{ association: "Image_products" }],
+                order: [["createdAt", "DESC"]],
+            });
+
+            const categories = await db.Category_product.findAll({
+                include: [
+                    {
+                        model: db.Product,
+                        as: "Products",
+                        attributes: ["id", "name"],
+                    },
+                ],
             });
 
             const products = await db.Product.findAll();
 
-        let categoriaConMasProductos;
-        let maximoProductos = 0;
+            let categoriaConMasProductos;
+            let maximoProductos = 0;
 
-        categories.forEach(category => {
-            if (category.Products.length > maximoProductos) {
-                maximoProductos = category.Products.length;
-                categoriaConMasProductos = category;
-            }
-        });
+            categories.forEach((category) => {
+                if (category.Products.length > maximoProductos) {
+                    maximoProductos = category.Products.length;
+                    categoriaConMasProductos = category;
+                }
+            });
 
-                // ACA CONSUMO LA API DE USER PARA UTILIZAR EL COUNT Y SABER CUANTOS USUARIOS EXISTEN.
-                const usersCount = await fetch('http://localhost:3001/api/allUsers');
-                const countApi = await usersCount.json();
-                const allUsers = countApi.meta.count;
+            // le pido a la api que me envie el count de los productos con la api
+            const productsCountResponse = await fetch("http://localhost:3000/api/products");
+            const productsCountData = await productsCountResponse.json();
+            const allProductsCount = productsCountData.meta.count;
 
-        res.render("./dashboardReact/dashboardReact", {title: "Dashboard",
-        usuarioLogeado: req.session.usuarioLogin,
-        product: ultimoProducto,
-        categories: categories,
-        maxCategories: categoriaConMasProductos,
-        products:products,
-        count:allUsers
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Hubo un error en el panel de control.");
-    }
+            // le pido a la api que me envie el count de los usuarios con la api
+            const usersCountResponse = await fetch("http://localhost:3000/api/allUsers");
+            const usersCountData = await usersCountResponse.json();
+            const allUsers = usersCountData.meta.count;
 
-},
+            
+            const categoriesCount = categories.length;// count de categorías
 
+            res.render("./dashboardReact/dashboardReact", {
+                title: "Dashboard",
+                usuarioLogeado: req.session.usuarioLogin,
+                product: ultimoProducto,
+                categories: categories,
+                maxCategories: categoriaConMasProductos,
+                products: products,
+                productsCount: allProductsCount,
+                usersCount: allUsers,
+                categoriesCount: categoriesCount,
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(500).send("Hubo un error en el panel de control.");
+        }
+    },
+};
 
-}
+module.exports = dashboardController;
 
-
-module.exports = usersController;
