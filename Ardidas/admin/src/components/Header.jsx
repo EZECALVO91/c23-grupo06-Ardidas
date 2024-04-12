51fcb144f8de1dfca8892592be7bab76738b118b
EZECALVO91 <ezequielcalvo@live.com>
2024-04-12 15:32:53 -0300

    Leo termino de completar el mapeo de los botones para el dashboard de React y yo le termine de agregar la logica a los botones con esto ya tendriamos funcionando el dashboard de react y esta tal cual lo arme en express, mas que contento.

--- Ardidas/admin/src/components/Header.jsx
+++ Ardidas/admin/src/components/Header.jsx
@@ -16,9 +16,9 @@ export default function Header () {
                     <li>
                         <a className="" href="http://localhost:3000/"><i className="fa-solid fa-house"></i></a>
                     </li>
-                    <li>
+                    {/* <li>
                         <a className="" href="http://localhost:3000/products"><i className="fa-solid fa-boxes-stacked "></i></a>
-                    </li>
+                    </li> */}
                 </ul>
             </nav>
 
